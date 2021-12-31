b63937dac61043c5d9d27261387638b0c2c20124
Tom Verbeure <hombre+github.com@gmail.com>
2021-12-30 16:56:16 -0800

    Suppress GCC extension warning.

--- sw/riscv.h
+++ sw/riscv.h
@@ -86,16 +86,16 @@
 
 
 #define csr_swap(csr, val)					\
-({								\
-	uint32_t __v = (uint32_t)(val);		\
+__extension__ ({						\
+	uint32_t __v = (uint32_t)(val);				\
 	__asm__ __volatile__ ("csrrw %0, " #csr ", %1"		\
 			      : "=r" (__v) : "rK" (__v));	\
 	__v;							\
 })
 
 #define csr_read(csr)						\
-({								\
-	uint32_t __v;				\
+__extension__ ({						\
+	uint32_t __v;						\
 	__asm__ __volatile__ ("csrr %0, " #csr			\
 			      : "=r" (__v));			\
 	__v;							\
@@ -103,14 +103,14 @@
 
 #define csr_write(csr, val)					\
 {								\
-	uint32_t __v = (uint32_t)(val);		\
+	uint32_t __v = (uint32_t)(val);				\
 	__asm__ __volatile__ ("csrw " #csr ", %0"		\
 			      : : "rK" (__v));			\
 }
 
 #define csr_read_set(csr, val)					\
-({								\
-	uint32_t __v = (uint32_t)(val);		\
+__extension__ ({						\
+	uint32_t __v = (uint32_t)(val);				\
 	__asm__ __volatile__ ("csrrs %0, " #csr ", %1"		\
 			      : "=r" (__v) : "rK" (__v));	\
 	__v;							\
@@ -118,14 +118,14 @@
 
 #define csr_set(csr, val)					\
 {								\
-	uint32_t __v = (uint32_t)(val);		\
+	uint32_t __v = (uint32_t)(val);				\
 	__asm__ __volatile__ ("csrs " #csr ", %0"		\
 			      : : "rK" (__v));			\
 }
 
 #define csr_read_clear(csr, val)				\
-({								\
-	uint32_t __v = (uint32_t)(val);		\
+__extension__ ({						\
+	uint32_t __v = (uint32_t)(val);				\
 	__asm__ __volatile__ ("csrrc %0, " #csr ", %1"		\
 			      : "=r" (__v) : "rK" (__v));	\
 	__v;							\
@@ -133,7 +133,7 @@
 
 #define csr_clear(csr, val)					\
 {								\
-	uint32_t __v = (uint32_t)(val);		\
+	uint32_t __v = (uint32_t)(val);				\
 	__asm__ __volatile__ ("csrc " #csr ", %0"		\
 			      : : "rK" (__v));			\
 }
